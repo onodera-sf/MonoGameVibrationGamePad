f61cae1e419f97634a876c340e9e0ce17ed73abf
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-06-15 09:05:30 +0900

    プロジェクト ファイルを追加します。

---
+++ VibrationGamePad/Game1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace VibrationGamePad
+{
+	/// <summary>
+	/// ゲームメインクラス
+	/// </summary>
+	public class Game1 : Game
+	{
+    /// <summary>
+    /// グラフィックデバイス管理クラス
+    /// </summary>
+    private readonly GraphicsDeviceManager _graphics = null;
+
+    /// <summary>
+    /// スプライトのバッチ化クラス
+    /// </summary>
+    private SpriteBatch _spriteBatch = null;
+
+    /// <summary>
+    /// スプライトでテキストを描画するためのフォント
+    /// </summary>
+    private SpriteFont _font = null;
+
+    /// <summary>
+    /// ゲームパッドの状態
+    /// </summary>
+    private GamePadState _gamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+    /// <summary>
+    /// GameMain コンストラクタ
+    /// </summary>
+    public Game1()
+    {
+      // グラフィックデバイス管理クラスの作成
+      _graphics = new GraphicsDeviceManager(this);
+
+      // ゲームコンテンツのルートディレクトリを設定
+      Content.RootDirectory = "Content";
+
+      // マウスカーソルを表示
+      IsMouseVisible = true;
+    }
+
+    /// <summary>
+    /// ゲームが始まる前の初期化処理を行うメソッド
+    /// グラフィック以外のデータの読み込み、コンポーネントの初期化を行う
+    /// </summary>
+    protected override void Initialize()
+    {
+      // TODO: ここに初期化ロジックを書いてください
+
+      // コンポーネントの初期化などを行います
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// ゲームが始まるときに一回だけ呼ばれ
+    /// すべてのゲームコンテンツを読み込みます
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // テクスチャーを描画するためのスプライトバッチクラスを作成します
+      _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      // フォントをコンテンツパイプラインから読み込む
+      _font = Content.Load<SpriteFont>("Font");
+    }
+
+    /// <summary>
+    /// ゲームが終了するときに一回だけ呼ばれ
+    /// すべてのゲームコンテンツをアンロードします
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: ContentManager で管理されていないコンテンツを
+      //       ここでアンロードしてください
+    }
+
+    /// <summary>
+    /// 描画以外のデータ更新等の処理を行うメソッド
+    /// 主に入力処理、衝突判定などの物理計算、オーディオの再生など
+    /// </summary>
+    /// <param name="gameTime">このメソッドが呼ばれたときのゲーム時間</param>
+    protected override void Update(GameTime gameTime)
+    {
+      // ゲームパッドの情報を取得
+      _gamePadState = GamePad.GetState(PlayerIndex.One);
+
+      // ゲームパッドの Back ボタン、またはキーボードの Esc キーを押したときにゲームを終了させます
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      {
+        Exit();
+      }
+
+      // トリガーにあわせてコントローラーを振動させる
+      GamePad.SetVibration(PlayerIndex.One,
+          _gamePadState.Triggers.Left, _gamePadState.Triggers.Right);
+
+      // 登録された GameComponent を更新する
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// 描画処理を行うメソッド
+    /// </summary>
+    /// <param name="gameTime">このメソッドが呼ばれたときのゲーム時間</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      // 画面を指定した色でクリアします
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      // スプライトの描画準備
+      _spriteBatch.Begin();
+
+      // 左トリガー
+      _spriteBatch.DrawString(_font,
+          "LeftTrigger  : " + _gamePadState.Triggers.Left.ToString("f8"),
+          new Vector2(20.0f, 30.0f), Color.White);
+
+      // 右トリガー
+      _spriteBatch.DrawString(_font,
+          "RIghtTrigger : " + _gamePadState.Triggers.Right.ToString("f8"),
+          new Vector2(20.0f, 60.0f), Color.White);
+
+      // スプライトの一括描画
+      _spriteBatch.End();
+
+      // 登録された DrawableGameComponent を描画する
+      base.Draw(gameTime);
+    }
+  }
+}
